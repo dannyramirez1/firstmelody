35fd6b24a11dac5c62a4aae7b82637a779a0ff54
dannyramirez1 <37849344+dannyramirez1@users.noreply.github.com>
2020-01-21 14:24:07 -0800

    Add files via upload

---
+++ thehills.rb
@@ -0,0 +1,121 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 113
+use_synth :piano
+3.times do
+  play :d4, sustain: 2
+  sleep 3
+  play :c5
+  sleep 1
+  play :d4, sustain: 2
+  sleep 2
+end
+2.times do
+  play :f3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :f3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :f3
+  sleep 1
+  play :e3
+  sleep 1
+  sleep 2
+end
+
+play :f3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :f3
+sleep 0.5
+play :e3
+sleep 0.5
+play :f3
+sleep 1
+play :e3
+sleep 1
+sleep 1
+sleep 0.5
+play:f3
+sleep 0.5
+2.times do
+  play :f3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :g3
+  sleep 0.5
+  play :f3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :f3
+  sleep 1
+  play :e3
+  sleep 1
+  sleep 2
+end
+play :f3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :g3
+sleep 0.5
+play :f3
+sleep 0.5
+play :e3
+sleep 0.5
+play :f3
+sleep 1
+play:g3
+sleep 0.5
+play:f3
+sleep 0.5
+sleep 2
+play:g3
+sleep 0.5
+play:g3
+sleep 0.5
+play:g3
+sleep 0.5
+play:g3
+sleep 0.5
+play:a3
+sleep 0.5
+play:g3
+sleep 0.5
+play:f3
+sleep 0.5
+play:e3
+sleep 0.5
